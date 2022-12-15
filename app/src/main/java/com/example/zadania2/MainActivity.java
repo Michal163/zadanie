af914d0c659588fd935bf83b9dacbb1d4d3934c7
Michal163 <90699775+Michal163@users.noreply.github.com>
2022-12-15 13:36:28 +0100

    -

---
+++ app/src/main/java/com/example/zadania2/MainActivity.java
@@ -0,0 +1,176 @@
+package com.barstool.zadanie2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private ImageView img;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    Button bt;
+    Switch lok;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Zmienne
+        img = findViewById(R.id.image);
+        bt = findViewById(R.id.button);
+        lok = findViewById(R.id.lokalizacja);
+        txt = findViewById(R.id.text);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        //GPS
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                //Wypisanie aktualnych koordynatow
+                try{
+                    Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+                    String adres = addresses.get(0).getAddressLine(0);
+                    txt.setText("\n Koordynaty: " + location.getLatitude() + " , " + location.getLongitude() + "\n Adres: " + adres);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+        //Permisje do GPS, pyta sie o pozwolenie
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10 );
+            return;
+        }else{
+            configureButton();
+        }
+
+
+        bt.setEnabled(false);
+
+        //Wyswietlenie pozwolenia do uzycia kamery
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                MY_CAMERA_PERMISSION_CODE);
+
+
+        //Aktywowanie Kamery po przez przycisk
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+    }
+
+    //Wywolanie koordynatow po przez switcha
+    private void configureButton() {
+        lok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                if(lok.isChecked()==true)
+                    txt.setVisibility(View.VISIBLE);
+                else
+                    txt.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    //Nadanie permisji i dodanie stosownego Toast'a
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        //permisje kamera
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                bt.setEnabled(true);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                bt.setEnabled(false);
+            }
+        }
+        //Permisje GPS
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    configureButton();
+                    Toast.makeText(this, "GPS permission granted", Toast.LENGTH_LONG).show();
+                    lok.setEnabled(true);
+                }else{
+                    Toast.makeText(this, "GPS permission denied", Toast.LENGTH_LONG).show();
+                    bt.setEnabled(false);
+                }
+                return;
+        }
+    }
+
+    //Wyswietlenie zdjecia z kamery w aplikacji
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(photo);
+        }
+    }
+}
